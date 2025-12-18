68e7712d68dfd8f332b29474dd930f6a3086df5f
Andrei <andreihson@proton.me>
2025-12-18 12:07:43 -0800

    Serial works!

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
+#include <stdint.h>
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+  static uint64_t n_blinks;
+
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
+  Serial.printf("%lu\n", n_blinks++);
 }
 
